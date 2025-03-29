0d9bb7237b8c78039d93bad8545091fc7313e47e
luisxavierxd <luisxavierxd@gmail.com>
2025-03-28 18:48:41 -0600

    a

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -7,8 +7,8 @@ def generate_launch_description():
     return LaunchDescription([
 
         Node(
-            package='ldlidar_node',
-            executable='ldlidar_bringup.launch.py',
+            package='rplidar_ros',
+            executable='rplidar_composition',
             output='screen',
             parameters=[{
                 'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
